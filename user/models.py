d6a0b3e202e61dcb12fa3db692480bcdc483e084
anwar4en <anwar4en@gmail.com>
2024-01-01 20:49:46 +0300

    updated user model

--- user/models.py
+++ user/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 from roles.models import Role
 # Create your models here.
 class User(AbstractUser):
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    # role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
---
+++ user/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        # fields = ['username', 'email', 'password' , "first_name" , "last_name"]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create_superuser(self, validated_data):        
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        # user.is_staff = validated_data['is_staff']
+        # user.is_superuser = validated_data['is_superuser']
+        user.save()
+        return user
