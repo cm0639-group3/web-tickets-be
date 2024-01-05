725897e243c7367398a02e1ea5a79bf1ffc4d78d
euphonie <cs.joel.09@gmail.com>
2024-01-06 00:20:35 +0100

    BE-CRUD-002.2: add flight model

--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Flight
+from .serializers import FlightSerializer
+from .permissions import FlightPermissions
 
-# Create your views here.
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    permission_classes = [FlightPermissions,]
