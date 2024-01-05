57d77d0cdfd22c593c1ae7eac3c39262c0f8d8f7
anwar4en <anwar4en@gmail.com>
2024-01-06 00:11:30 +0300

    Implemented Airplane app

--- airplane/models.py
+++ airplane/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from airline.models import Airline
 # Create your models here.
 class Airplane(models.Model):
-    name = models.CharField(255)
+    name = models.CharField(max_length=255)
     seats = models.IntegerField()
     model_number = models.CharField(max_length=10, unique=True)
     airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
--- flightManager/settings.py
+++ flightManager/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'airline',
-    # 'airplane',
+    'airplane',
     'airport',
     # 'cart',
     'city',
