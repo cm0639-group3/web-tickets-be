b4583353b088d35f67a5e40fef83993e0e40a359
euphonie <cs.joel.09@gmail.com>
2024-01-06 00:20:35 +0100

    BE-CRUD-002.2: add flight model

---
+++ flight/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+class FlightPermissions(permissions.BasePermission):
+    def has_permission(self, request, view) :
+        if view.action == "create":
+            return request.user.is_authenticated and request.user.is_staff
+        if view.action == "list":
+            return True
+        elif view.action in ["retrieve"]:
+            return True
+        elif view.action in ["update", "partial_update"]:
+            return request.user.is_authenticated and request.user.is_staff
+        elif view.action in ["destroy"]:
+            return request.user.is_authenticated and request.user.is_staff
+        else:
+            return False
