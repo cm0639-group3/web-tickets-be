43dd3388b948f8a51ceca92f65e6d032e2d81de6
anwar4en <anwar4en@gmail.com>
2024-01-05 23:19:43 +0300

    Implemented Role model

---
+++ role/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# -2dL (will delete later)
+from .models import Role
+# Register your models here.
+admin.site.register(Role)
--- user/models.py
+++ user/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from roles.models import Role
+from role.models import Role
 # Create your models here.
 class User(AbstractUser):
-    # role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE , default="1")
     email = models.EmailField(unique=True)
 
     def __str__(self):
