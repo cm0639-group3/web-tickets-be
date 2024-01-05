7dee7ba2d48d8505e2fa8efca140811bd0c785ec
anwar4en <anwar4en@gmail.com>
2024-01-05 23:39:17 +0300

    Implemented Airline app

--- airline/views.py
+++ airline/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Airline
+from .serializers import AirlineSerializer
+from .permissions import CustomPermissions_1
+from rest_framework.permissions import  AllowAny, IsAuthenticated , IsAdminUser
 
-# Create your views here.
+class AirlineViewSet(viewsets.ModelViewSet):
+    queryset = Airline.objects.all()
+    serializer_class = AirlineSerializer
+    permission_classes = [CustomPermissions_1,]
--- role/admin.py
+++ role/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-# -2dL (will delete later)
 from .models import Role
 # Register your models here.
 admin.site.register(Role)
