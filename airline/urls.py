b0de30e9303d62240ace13a9f490495b777d7e57
KyleJYKim <kimjuyoung656@icloud.com>
2024-01-08 17:53:41 +0100

    added get_queryset()

--- airline/urls.py
+++ airline/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AirlineViewSet
+from .views import AirlineViewSet, AirlineListAPIView
 
 router = DefaultRouter()
 router.register(r'', AirlineViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('airlines/', AirlineListAPIView.as_view(), name='airline-list'),
 ]
--- airline/views.py
+++ airline/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Airline
-from rest_framework import status
 from .serializers import AirlineSerializer
 from .permissions import AirlinePermissions
 
@@ -14,10 +15,19 @@ class AirlineViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
+        headers = self.get_succsdess_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+    
+class AirlineListAPIView(generics.ListAPIView):
+    serializer_class = AirlineSerializer
+
+    def get_queryset(self):
+        city_name = self.request.query_params.get('city', '')
+        if city_name:
+            return Airline.objects.filter(city__name__icontains=city_name)
+        return Airline.objects.all()
